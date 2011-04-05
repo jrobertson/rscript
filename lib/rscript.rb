ff3d11abf8b87c183857e44b81727e13b979fd03
Your Name <james@r0bertson.co.uk>
2011-04-05 14:58:57 +0100

    reinstatedthecachefeature

--- lib/rscript.rb
+++ lib/rscript.rb
@@ -22,11 +22,12 @@
 
 
 require 'rscript_base'
+require 'hashcache'
 
 class RScript < RScriptBase
 
   def initialize()
-
+    @rsf_cache = HashCache.new(cache: 5)
   end
   
   def read(args=[])
@@ -56,6 +57,9 @@ class RScript < RScriptBase
     [out, args]
   end
 
+  def reset()
+    @rsf_cache.reset
+  end
   
   private
 
@@ -65,7 +69,7 @@ class RScript < RScriptBase
     $rsfile = rsfile[/[a-zA-z0-9]+(?=\.rsf)/]
 
     @url_base = rsfile[/\w+:\/\/[^\/]+/]
-    buffer = read_sourcecode(rsfile)
+    buffer = @rsf_cache.read(rsfile) {read_sourcecode(rsfile) }
 
     doc =  Document.new(buffer)
     yield(doc)
@@ -77,7 +81,7 @@ class RScript < RScriptBase
     rsfile = args[0]; args.shift
 
     $rsfile = rsfile[/[a-zA-z0-9]+(?=\.rsf)/]
-    buffer = read_sourcecode(rsfile) 
+    buffer = @rsf_cache.read(rsfile) {read_sourcecode(rsfile) }
     @url_base = rsfile[/\w+:\/\/[^\/]+/]
     doc =  Document.new(buffer)
     yield(doc)
--- rscript.gemspec
+++ rscript.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'rscript'
-  s.version = '0.1.10'
+  s.version = '0.1.11'
   s.summary = 'rscript previously known as rcscript, reads Ruby code which is embedded within an rscript XML file.'
   s.files = Dir['lib/**/*.rb']
   s.add_dependency('hashcache')
